d7ce4eabf62a705e3025f797d33b6dab88806c7a
hitsuji-haneta <hitsuji.haneta1986@gmail.com>
2019-02-03 21:00:36 +0900

    update 'modify' logic in upsert

--- contracts/addressbook/addressbook.cpp
+++ contracts/addressbook/addressbook.cpp
@@ -34,18 +34,48 @@ class [[eosio::contract]] addressbook : public eosio::contract {
           row.state = state;
         });
         send_summary(user, " successfully emplaced record to addressbook");
+        increment_counter(user, "emplace");
       } else {
         std::string changes;
         addresses.modify(iterator, user, [&]( auto& row ) {
-          row.key = user;
-          row.first_name = first_name;
-          row.last_name = last_name;
-          row.age = age;
-          row.street = street;
-          row.city = city;
-          row.state = state;
+
+          if(row.first_name != first_name) {
+            row.first_name = first_name;
+            changes += "first name ";
+          }
+
+          if(row.last_name != last_name) {
+            row.last_name = last_name;
+            changes += "last name ";
+          }
+
+          if(row.age != age) {
+            row.age = age;
+            changes += "age ";
+          }
+
+          if(row.street != street) {
+            row.street = street;
+            changes += "street ";
+          }
+
+          if(row.city != city) {
+            row.city = city;
+            changes += "city ";
+          }
+
+          if(row.state != state) {
+            row.state = state;
+            changes += "state ";
+          }
         });
-        send_summary(user, " successfully modified record to addressbook");
+
+        if(changes.length() > 0) {
+          send_summary(user, " successfully modified record in addressbook. Fields changed: " + changes);
+          increment_counter(user, "modify");
+        } else {
+          send_summary(user, " called upsert, but request resulted in no changes.");
+        }
       }
     }
 
@@ -57,6 +87,7 @@ class [[eosio::contract]] addressbook : public eosio::contract {
       eosio_assert(iterator != addresses.end(), "Record does not exist.");
       addresses.erase(iterator);
       send_summary(user, " successfully erased record from addressbook");
+      increment_counter(user, "erase");
     }
 
     [[eosio::action]]
@@ -91,6 +122,16 @@ class [[eosio::contract]] addressbook : public eosio::contract {
         "notify"_n,
         std::make_tuple(user, name{user}.to_string() + message)
       ).send();
+    }
+
+    void increment_counter(name user, std::string type) {
+      action counter = action(
+        permission_level{get_self(),"active"_n},
+        "abcounter"_n,
+        "count"_n,
+        std::make_tuple(user, type)
+      );
+      counter.send();
     }
 };
 
